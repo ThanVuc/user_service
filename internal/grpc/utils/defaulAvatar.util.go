647d6337bf273e2ea9c8436a9cafcf16ead1eb4c
Nguyen Duc Viet <ndviet1501@gmail.com>
2025-10-16 21:12:14 +0700

    [BE] SCD-127: fix avatars default

--- internal/grpc/utils/defaulAvatar.util.go
+++ internal/grpc/utils/defaulAvatar.util.go
@@ -1,9 +1,8 @@
 package utils
 
-
 const (
-	DefaultMaleAvatar = "https://pub-49aeefce5382467589ef388921404348.r2.dev/default/ea8a5596-8088-403e-9050-e30826a5a1db.webp"
-	DefaultFemaleAvatar = "https://pub-49aeefce5382467589ef388921404348.r2.dev/default/0cdaa125-f05c-4098-bb4d-d9ce183a6fbf.webp"
+	DefaultMaleAvatar   = "https://pub-49aeefce5382467589ef388921404348.r2.dev/default/ea8a5596-8088-403e-9050-e30826a5a1db.webp"
+	DefaultFemaleAvatar = "https://pub-49aeefce5382467589ef388921404348.r2.dev/default/79c38322-cf71-4f60-bbd5-ca6861bffcb7.webp"
 )
 
 func GetDefaultAvatarUrl(isMale bool) (string, error) {
